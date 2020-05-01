02d077735e9cfa733729465897b18fc7604b3d4a
Giancarlo Ventura <khan.maytok@gmail.com>
2020-05-01 15:44:10 -0500

    :wrench: Config for secrets

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.signals import user_logged_out
 
-# Create your views here.
+
+def logout(request):
+    user = getattr(request, 'user', None)
+    if not getattr(user, 'is_authenticated', True):
+        user = None
+    user_logged_out.send(sender=user.__class__, request=request, user=user)
+    request.session.flush()
+    if hasattr(request, 'user'):
+        from django.contrib.auth.models import AnonymousUser
+        request.user = AnonymousUser()
+
+    return redirect('home')
