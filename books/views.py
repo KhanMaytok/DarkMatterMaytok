93652b2530e5258f070e5aad880f1ea890d2bfd9
Giancarlo Ventura <khan.maytok@gmail.com>
2020-05-04 01:43:09 -0500

    :wrench: Config for secrets

--- books/views.py
+++ books/views.py
@@ -7,14 +7,12 @@ from django.views.decorators.cache import cache_page
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
-@cache_page(CACHE_TTL)
 def book_show(request, pk=None, slug=None):
     book = get_object_or_404(Book, pk=pk)
 
     return render(request, 'books/book.html', {'book': book})
 
 
-@cache_page(CACHE_TTL)
 def chapter_show(request, pk=None, slug=None, chapter_number=1):
     book = get_object_or_404(Book, pk=pk)
     chapter = get_object_or_404(Chapter, book=book, chapter_number=chapter_number)
