a0e2132254c823ab50e3cd8cc3f11ec5a7f679e5
Giancarlo Ventura <khan.maytok@gmail.com>
2020-05-04 16:03:05 -0500

    :wrench: Config for secrets

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-
+from django.db import models
+from django.forms import Textarea
 from blog.models import Post
 
 
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('body',)
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(
+            attrs={'style': 'width: 100%;'})},
+    }
 
 
 admin.site.register(Post, PostAdmin)
