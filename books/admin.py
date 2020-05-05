151d7b82fd0389f21a3b7ef0e3d988f9012cc670
Giancarlo Ventura <khan.maytok@gmail.com>
2020-05-05 12:57:34 -0500

    :wrench: Config for secrets

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 from books.models import Book, Chapter
 
+
+class ChapterAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+
 admin.site.register(Book)
-admin.site.register(Chapter)
+admin.site.register(Chapter, ChapterAdmin)
