f1136ef90a948c2728d4b6d2e77f35cd84ba7570
Giancarlo Ventura <khan.maytok@gmail.com>
2020-05-10 20:40:53 -0500

    :wrench: Config for secrets

--- books/views.py
+++ books/views.py
@@ -20,8 +20,11 @@ def chapter_show(request, pk=None, slug=None, chapter_number=1):
     book = get_object_or_404(Book, pk=pk)
     chapter = get_object_or_404(Chapter, book=book, chapter_number=chapter_number)
 
+    if chapter.rank_required != 'C' and not request.user.is_authenticated:
+        return HttpResponse("El capítulo está en estado Beta. Solo miembros con acceso pueden leerlo.")
+
     # Check if the user has permissions
-    if not request.user.is_authenticated or not user_has_min_rank(request.user, chapter.rank_required):
+    if not user_has_min_rank(request.user, chapter.rank_required):
         return HttpResponse("Lo sentimos. Aún no tienes el rango para leer este capítulo.")
 
     return render(request, 'books/chapter.html', {'book': book, 'chapter': chapter})
