ab46b87f8814246db3aa5a80ab3094221c74ff90
Giancarlo Ventura <khan.maytok@gmail.com>
2020-05-23 00:28:57 -0500

    :wrench: Config for secrets

--- DarkMatterMaytok/settings.py
+++ DarkMatterMaytok/settings.py
@@ -177,6 +177,7 @@ SOCIAL_AUTH_POSTGRES_JSONFIELD = True
 
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 ]
 
