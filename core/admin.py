8478b7bc3d099980b1ac79ab615c0dc3669d5aa0
Giancarlo Ventura <khan.maytok@gmail.com>
2020-10-02 15:51:06 -0500

    :art: Revert comments in another admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from django_comments.models import Comment
-from django_comments_xtd.models import XtdComment
-
 from core.models import User
 
 admin.site.register(User)
-admin.site.register(Comment)
-admin.site.register(XtdComment)
