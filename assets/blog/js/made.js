8eeee314d0bb861df659d4139ac43b95babb1341
Giancarlo Ventura <khan.maytok@gmail.com>
2021-12-22 17:24:39 -0500

    Update some libraries

--- assets/blog/js/made.js
+++ assets/blog/js/made.js
@@ -328,25 +328,7 @@
                     rewind: true
                 });
                 $this.on('translated.owl.carousel', function (event) {
-                    //changeSubtitle(event);
-                    let t = $(event.target).find('.owl-item.active a').first();
-                    let name = t.attr('data-book-name');
-                    console.log('translated');
-                    console.log(name);
-                });
-                $this.on('change.owl.carousel', function (event) {
-                    //changeSubtitle(event);
-                    let t = $(event.target).find('.owl-item.active a').first();
-                    let name = t.attr('data-book-name');
-                    console.log('change');
-                    console.log(name);
-                });
-                $this.on('changed.owl.carousel', function (event) {
-                    //changeSubtitle(event);
-                    let t = $(event.target).find('.owl-item.active a').first();
-                    let name = t.attr('data-book-name');
-                    console.log('changed');
-                    console.log(name);
+                    changeSubtitle(event);
                 });
             });
         });
