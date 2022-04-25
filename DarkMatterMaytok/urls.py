ee086479960bf58f82ec81e1872ef78ae9651778
Giancarlo Ventura <khan.maytok@gmail.com>
2022-04-24 22:44:30 -0500

    Superuser can look at draft posts

--- DarkMatterMaytok/urls.py
+++ DarkMatterMaytok/urls.py
@@ -59,7 +59,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('summernote/', include('django_summernote.urls')),
-
 ]
 
 if settings.DEBUG is True:
