2ea1939c4061cf9b8fc104ead1fae309419e5107
Giancarlo Ventura <khan.maytok@gmail.com>
2022-05-05 07:16:45 -0500

    Random fund 10 members

--- blog/management/commands/create_fake_user.py
+++ blog/management/commands/create_fake_user.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         username = kwargs['username']
-        user = User.objects.create_user(username=username, password=uuid.uuid4(),
+        user = User.objects.create_user(username=username, password=uuid.uuid4().__str__(),
                                         email=f'{username}@{username}.{username}')
         user.is_founder = True
         user.save()
