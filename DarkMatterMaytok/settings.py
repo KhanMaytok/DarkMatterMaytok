e487dfe1bbe288559d741b69773b58724ce1aaf5
Giancarlo ventura <khan.maytok@gmail.com>
2022-11-24 00:29:39 -0500

    Exclude fields on wiki admin

--- DarkMatterMaytok/settings.py
+++ DarkMatterMaytok/settings.py
@@ -310,3 +310,36 @@ if SHOW_QUERIES is True:
     }
 
 PAGE_SIZE = 7
+
+LOG_TO_FILE = config('LOG_TO_FILE', cast=bool, default=False)
+
+if LOG_TO_FILE is True:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "root": {"level": "INFO", "handlers": ["file"]},
+        "handlers": {
+            "file": {
+                "level": "INFO",
+                "class": "logging.FileHandler",
+                "filename": BASE_DIR / "django.log",
+                "formatter": "app",
+            },
+        },
+        "loggers": {
+            "django": {
+                "handlers": ["file"],
+                "level": "INFO",
+                "propagate": True
+            },
+        },
+        "formatters": {
+            "app": {
+                "format": (
+                    u"%(asctime)s [%(levelname)-8s] "
+                    "(%(module)s.%(funcName)s) %(message)s"
+                ),
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+        },
+    }
